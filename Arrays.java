fff4accf6acddd48f9f964ffbbc1eec9c2226b41
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-23 21:50:40 +0530

    Java Arrays added

---
+++ Arrays.java
@@ -0,0 +1,46 @@
+public class Arrays{
+
+
+public static void main(String[] args) {
+    
+
+    int [] books = new int[6];
+
+  //or use 
+
+     int [] books2 = {11,12,13,14};
+
+
+   int[] books3; //declaration  of arrays,books3 getting defined in stack
+
+   books3 = new int[8]; //initialization-actually here objects created in heap memory
+
+ 
+ 
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
