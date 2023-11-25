a94db8e225088ba332788c96462f74c8d59fd52f
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-25 13:55:56 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Arrays{
 
 
 public static void main(String[] args) {
     
+
+   Scanner sc = new Scanner(System.in);
+
+
+
+
+   
 
     int [] books = new int[6];
 
@@ -19,28 +28,47 @@ public static void main(String[] args) {
  
 
 
+   int[] arr = new int[5];
+
+   arr[0] = 0;
+   arr[1] = 10;
+   arr[2] = 20;
+   arr[3] = 30;
+   arr[4]  = 40;
 
 
+   System.out.println(arr[0] + arr[1]); // adding two array elements
+
+   
+
+    
+   //using for loop to get elemnts form an user for  an array
+   
 
 
-}
+   for(int i = 0; i < arr.length; i++){
+           
+          System.out.println("Enter integer for " + i + " index");
+          arr[i] = sc.nextInt();  
+
+   } 
 
 
+ //using for loop to print elemnts in the array
 
+ for(int i = 0; i < arr.length;i++){
 
+       System.out.println(arr[i]);
 
+ }
 
+// also we can simply use for each loop to print element in the array
 
+   for(int num : arr) { 
 
+    System.out.println(num);
 
+  }
 
-
-
-
-
-
-
-
-
-
+    }
 }
