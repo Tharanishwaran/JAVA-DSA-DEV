19d98ecf863c84e2affc22a63170376eb7ebaa1b
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-26 19:37:07 +0530

    arraylist file added

---
+++ ArraylistNote.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class ArraylistNote {
+public static void main(String[] args) {
+    
+
+    Scanner input = new Scanner(System.in);
+
+    ArrayList<Integer> list = new ArrayList<>(5);
+
+    list.add(220);
+    list.add(230);
+    list.add(240);
+    list.add(250);
+    list.add(260);
+    list.add(270);
+    list.add(280);
+    list.add(290);
+
+    System.out.println(list);
+
+    list.set(1,24);    
+    list.remove(3);
+
+
+
+    System.out.println(list);
+
+
+   for (int i = 0; i < list.size(); i++) {
+
+
+    
+
+   }
+
+ 
+    
+
+
+
+}
+
+
+
+
+
+
+
+    
+}
