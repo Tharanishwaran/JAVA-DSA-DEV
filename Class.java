ba67b3a341e08481bae61cb621766b3603f782ae
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-29 20:29:48 +0530

    class file added

---
+++ Class.java
@@ -0,0 +1,54 @@
+package oops;
+
+public class Class {
+    
+
+public static void main(String[] args) {
+    
+
+    Cars car; //declaration of obj also known as refernce variable
+
+    car = new Cars(); //initializtion of obj 
+
+     
+    System.out.println(car.nameofcar);
+     System.out.println(car.price);
+    System.out.println(car.airbags);
+    System.out.println(car.speed);
+    System.out.println(car.modelno);
+
+}
+   
+
+
+
+
+
+}
+
+
+
+class Cars{
+
+String nameofcar = "audi"; //instance - is known as variables inside the class
+
+int modelno = 5110;
+
+float price = 10000.00f;
+
+int speed =  50;
+
+boolean airbags = true;
+
+
+
+}
+
+
+
+
+
+
+
+
+
