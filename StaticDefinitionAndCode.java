9daab808a0b19d228aa32b4d64bf2ce1547f7c2b
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-05 21:56:02 +0530

    Update StaticDefinitionAndCode.java

--- StaticDefinitionAndCode.java
+++ StaticDefinitionAndCode.java
@@ -1,1 +1,19 @@
+public class StaticDefinitonAndCode {
 
+public static void main(String[] args){ //main method is static because we use this method withou creating obeject
+
+
+// method1(); we can not access this method because we can not use non static method or variable iin staic method
+  
+
+
+}
+
+void method1(){
+
+  System.out.println("hi i am method 1 ");
+  
+}
+  
+
+}
