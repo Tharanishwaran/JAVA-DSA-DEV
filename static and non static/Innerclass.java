f19f5a427ee54a5da226fe345caa5d5c395ac7d0
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-01-03 13:08:52 +0530

    Add files via upload

---
+++ static and non static/Innerclass.java
@@ -0,0 +1,28 @@
+public class Innerclass {
+
+     static class Test1{
+
+       String name;
+
+      public Test1(String name){
+        
+        this.name =  name;
+
+      }
+
+    }
+
+    public static void main(String[] args) {
+        
+    
+        Test1 obj1 = new Test1("ak");
+
+        // System.out.println(obj1.name);
+
+        Test1 obj2 = new Test1("akash");
+
+        System.out.println(obj1.name);
+        System.out.println(obj2.name);
+
+    }
+}
