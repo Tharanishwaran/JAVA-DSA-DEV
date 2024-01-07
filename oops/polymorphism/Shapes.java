704a4e26a2bd8c7733eb02718b2a99ce139032db
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-01-07 21:37:53 +0530

    Add files via upload

---
+++ oops/polymorphism/Shapes.java
@@ -0,0 +1,26 @@
+package polymorphism;
+
+public class Shapes {
+
+void area(){
+
+    System.out.println("i am in shapes");
+}
+
+// @Override
+static void Tri(){
+      
+    System.out.println("berumuda traingle in shapes");
+
+
+ }
+
+
+ static void pyr(){
+
+  System.out.println("pyramids are in shapes");
+
+ }
+
+    
+}
