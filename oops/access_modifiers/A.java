663ec31b307a306fd191552c8c76e0e347c8eb86
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-01-07 21:34:41 +0530

    Add files via upload

---
+++ oops/access_modifiers/A.java
@@ -0,0 +1,21 @@
+package access_modifiers;
+
+public class A {
+    
+   public int num; //using public keyword we can access outside the pakagae and outside the file and laso inside the file
+   
+    private String name; //using purivate keyword we can access only inside the file
+
+   double numer; // without using any keyword than that is mean that deafult keyword so we only use this inside the pakage
+
+   protected float fractionalnum; //using protected keyword we can use outside the pakage when the outside pakage file extends the protected file expect this we can use this like public
+     
+
+
+    
+
+
+
+
+
+}
