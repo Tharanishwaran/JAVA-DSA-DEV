02e725f4a7bd681e147c8538482d93e195f51b4e
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-01-09 20:52:41 +0530

    Add files via upload

---
+++ oops/intefaces/Brake.java
@@ -0,0 +1,8 @@
+package intefaces;
+
+public interface Brake {
+
+    void brake();
+ 
+    
+}
---
+++ oops/intefaces/Main.java
@@ -0,0 +1,26 @@
+package intefaces;
+
+public class Main {
+
+
+public static void main(String[] args) {
+    
+
+  Car car = new Car();
+
+//   car.start();
+  car.brake();
+
+  // car.a;
+    
+
+ Supercar scar = new Supercar();
+
+  scar.start();
+ scar.stopMusic();
+ scar.upgradeEngine();
+  
+
+
+}
+}
