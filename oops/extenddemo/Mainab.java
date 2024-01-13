f46ed2956a096310b8d63ee0bf5fca153dda2a53
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-01-13 22:14:00 +0530

    extend added

---
+++ oops/extenddemo/Mainab.java
@@ -0,0 +1,22 @@
+package extenddemo;
+
+public class Mainab implements B {
+
+// @Override    
+// public void fun(){
+
+// }
+
+@Override
+public void greeting() {
+   
+}
+
+@Override
+public void fun() {
+   
+}
+
+
+    
+}
